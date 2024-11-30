4b938aa89f202607aa75ad13ac963ee2efdfa812
nachiketsoni <nxchikxt@gmail.com>
2024-12-01 02:35:15 +0530

    cors

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Update with your API base URL
-});
+    baseURL: 'https://todo-server-nine-psi.vercel.app/api',  // Your backend API
+    headers: {
+      'Content-Type': 'application/json',
+      
+    },
+
+    withCredentials: true,  // Send cookies with the request (if needed, e.g., for session authentication)
+  });
 
 // Request Interceptor
 axiosInstance.interceptors.request.use(
